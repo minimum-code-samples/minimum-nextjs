8976f22fc7acd1002739381676a23f14f7a4e61f
CH Chua <chuacheehow@gmail.com>
2023-09-11 00:56:09 +0800

    Refactored the SASS structure.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,8 +7,11 @@ export default function Home() {
         <title>Minimum Next.js</title>
         <meta name="description" content="Minimum code to create a Next.js project." />
       </Head>
-      <main>
+      <main className="mt-5">
         <h1 className="text-center">Minimum Next.js</h1>
+        <div className="text-center">
+          Built with <a href="https://nextjs.org">NEXT.js</a>
+        </div>
       </main>
     </>
   );
