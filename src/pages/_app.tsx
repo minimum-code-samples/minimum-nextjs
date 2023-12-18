c03fc4f555044ef124f509b8c3e451c8c2743332
CH Chua <chuacheehow@gmail.com>
2023-12-18 08:24:46 +0800

    Changed indentation from spaces to tabs.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,12 +4,12 @@ import Head from 'next/head';
 import '@src/styles/globals.scss';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+	return (
+		<>
+			<Head>
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,18 @@
 import Head from 'next/head';
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Minimum Next.js</title>
-        <meta name="description" content="Minimum code to create a Next.js project." />
-      </Head>
-      <main className="mt-5">
-        <h1 className="text-center">Minimum Next.js</h1>
-        <div className="text-center">
-          Built with <a href="https://nextjs.org">NEXT.js</a>
-        </div>
-      </main>
-    </>
-  );
+	return (
+		<>
+			<Head>
+				<title>Minimum Next.js</title>
+				<meta name="description" content="Minimum code to create a Next.js project." />
+			</Head>
+			<main className="mt-5">
+				<h1 className="text-center">Minimum Next.js</h1>
+				<div className="text-center">
+					Built with <a href="https://nextjs.org">NEXT.js</a>
+				</div>
+			</main>
+		</>
+	);
 }
